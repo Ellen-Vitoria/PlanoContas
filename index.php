f8854935c455ab698e6ae7919594ebbb13cfccc7
ellen <ellen.vitoria.ogs@gmail.com>
2021-06-30 02:04:53 -0300

    Inserindo Menu de Navegação

--- index.php
+++ index.php
@@ -7,9 +7,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" href="img/hand.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/main.css"> 
+
+    <!-- Bootstrap CSS -->
+    <link href="node_modules/bootstrap/dist/css/bootstrap-reboot.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>PlanoContas</title>
 </head>
 <body>
-    
+    <header>
+        <!-- Menu de Navegação - Nav bar -->
+        <nav>
+            <a class="logo"href="#"><img class="img-logo" src="img/hand.png" alt="PlanoContas Logo"> | PlanoContas</a>
+            <a class="navbar-btn" href="#">Entrar</a>
+        </nav>
+
+    </header>
+<script src="node_modules/jquery/dist/jquery.js"></script>   
 </body>
 </html>
